147d6baf9ffe6a9740d76fe356f3dc3fdb140b1a
tim <303433129@qq.com>
2023-03-16 18:42:45 +0800

    static check and logger bind

--- main.go
+++ main.go
@@ -106,6 +106,7 @@ func main() {
 		micro.Address(":9090"),
 		micro.Registry(reg),
 		micro.Name("go.micro.sever.worker"),
+		micro.WrapHandler(logWrapper(newLogger)),
 	)
 	service.Init()
 	gt.RegisterGreeterHandler(service.Server(), new(Greeter))
@@ -129,6 +130,22 @@ func HandleHTTP() {
 	}
 	// 启动http服务
 	http.ListenAndServe(":8080", mux)
+}
+
+// 使用go函数闭包的特性，对请求进行封装
+// 中间件函数在接收到GRPC请求时，可以打印出请求的具体参数，方便排查问题
+func logWrapper(log *zap.Logger) server.HandlerWrapper {
+	return func(fn server.HandlerFunc) server.HandlerFunc {
+		return func(ctx context.Context, req server.Request, rsp interface{}) error {
+			log.Info("receive request",
+				zap.String("method", req.Method()),
+				zap.String("service", req.Service()),
+				zap.Reflect("request param:", req.Body()),
+			)
+			err := fn(ctx, req, rsp)
+			return err
+		}
+	}
 }
 
 type Greeter struct {
